54b8565e61f9778ee3d063499b75836517d91d88
rick <rickyamas@gmail.com>
2022-10-28 02:53:13 -0300

    make fuction print_report

--- day17/main.py
+++ day17/main.py
@@ -1,6 +1,20 @@
+from data import resources 
 
+
+def print_report( ):
+	print(f"Water: {resources['water']}ml")
+	print(f"Milk: {resources['milk']}ml")
+	print(f"Coffee: {resources['coffee']}g")
+#	print(f"Money: ${profit}")
 
 is_on = True
 
 while is_on:
 	choice = input("What would you like? (espresso/latte/cappuccino):")
+	if choice == "off":
+		is_on = False
+	elif choice == "report":
+		print_report( )
+
+
+
